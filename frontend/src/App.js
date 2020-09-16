35654fd88836b7eae27b1805f14cd69a0b06c172
Arthur Santos <arthursantosgc@gmail.com>
2020-09-16 18:55:30 -0300

    projeto configurado para começar

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app">
+            <h1>MOVIE SEARCH</h1>
+
+            {/* header */}
+            {/* SearchBar */}
+            {/* MovieList */}
+        </div>
+    );
+}
+
+export default App;
