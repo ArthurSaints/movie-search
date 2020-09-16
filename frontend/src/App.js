36b0ad04da7ecb02737f56ba60552e7357ba5baa
Arthur Santos <arthursantosgc@gmail.com>
2020-09-16 19:14:32 -0300

    header finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
     return (
         <div className="app">
-            <h1>MOVIE SEARCH</h1>
-
-            {/* header */}
+            <Header />
             {/* SearchBar */}
             {/* MovieList */}
         </div>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Logo from "../images/logo-vermelho-preto.png";
+
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src={Logo} alt="muvy logo" />
+            <div className="header__title">FIND THE MOVIE WITH US!</div>
+        </div>
+    );
+}
+
+export default Header;
